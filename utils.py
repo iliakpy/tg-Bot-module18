d6748c9f43087a9f41c3619da58347393aa4359e
iliakpy <56565312+iliakpy@users.noreply.github.com>
2022-02-13 11:23:48 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,26 +2,29 @@ import requests
 import json
 from config import keys
 
+class ConvertionException(Exception):
+    pass
+
 
 class CryptoConverter():
     @staticmethod
     def convert(quote: str, base: str, amount: str):
         if quote == base:
-            return f'Невозможно перевести одинаковые валюты {base}.'
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            return f'Не удалось обработать валюту {quote}'
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            return f'Не удалось обработать валюту {base}'
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
 
         try:
             amount_float = float(amount)
         except ValueError:
-            return f'Не удалось обработать количество {amount}'
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
 
         print(json.loads(r.content))
